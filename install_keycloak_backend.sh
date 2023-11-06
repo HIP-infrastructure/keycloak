2a032c92dac3fe424c5795c87d03575dcd5d5178
Florian Sipp <florian.sipp@chuv.ch>
2023-11-06 11:02:36 +0000

    fix dependencies and keycloak hostname

--- install_keycloak_backend.sh
+++ install_keycloak_backend.sh
@@ -27,6 +27,20 @@ then
     echo "caddy installed."
 fi
 
+if ! command -v npm &> /dev/null
+then
+    echo "npm could not be found, installing..."
+    sudo apt-get update
+    sudo apt-get install -y ca-certificates curl gnupg
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+    NODE_MAJOR=21
+    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list 
+    sudo apt-get update
+    sudo apt-get install nodejs -y
+    echo "npm installed."
+fi
+
 if ! command -v pm2 &> /dev/null
 then
     echo "pm2 could not be found, installing..."
