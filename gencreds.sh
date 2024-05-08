20d69ee5ec0edbb54b651c008fb4af923a38170e
Yoan Blanc <yoan.blanc@chuv.ch>
2024-05-08 12:28:41 +0200

    feat: gencreds as python script
    It was shelling out to Python already
    
    Signed-off-by: Yoan Blanc <yoan.blanc@chuv.ch>

--- gencreds.sh
+++ gencreds.sh
@@ -11,6 +11,6 @@ echo -n "Enter keycloak_backend password: "
 read -rs keycloak_backend_password
 echo
 
-keycloak_backend_hash=`python3 -c "from werkzeug.security import generate_password_hash as g; print(g(\"$keycloak_backend_password\"), end=\"\");"`
+keycloak_backend_hash=`python3 -c "from werkzeug.security import generate_password_hash as g; print(g('$keycloak_backend_password', method='pbkdf2:sha256:600000'), end='');"`
 
 echo -n "$keycloak_backend_username@$keycloak_backend_hash" > ./keycloak_backend/keycloak_backend.secret
